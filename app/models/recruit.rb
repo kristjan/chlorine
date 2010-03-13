c6871b09cfe34c55ecbdb6e1dcee46813947d7aa
Kristján <kristjan@gmail.com>
2010-03-12 22:11:32 -0800

    Second flight. Workflow, scheduling

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -1,16 +1,26 @@
 class Recruit < ActiveRecord::Base
   has_many :feedbacks
-  has_many :activities
+  has_many :activities, :order => 'created_at desc'
 
   def status
-    "Pending"
+    if current_activity.terminal?
+      current_activity.friendly_name
+    elsif current_activity.scheduled?
+      current_activity.friendly_name
+    else
+      "Scheduling " + current_activity.friendly_name
+    end
   end
 
   def current_activity
-    activities.last(:order => :created_at)
+    @current_activity ||= activities.last(:order => :created_at)
+  end
+
+  def next_activity
+    Activity.next(current_activity)
   end
 
   after_create do |recruit|
-    Activity.received!(recruit)
+    Activity::Received.create!(:recruit => recruit, :scheduled_for => Time.now.utc)
   end
 end
