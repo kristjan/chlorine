c70c5bbce7a7d1269a8243a47e4c6a493adbd486
Kristján <kristjan@gmail.com>
2010-04-03 23:58:23 -0700

    Avoid copyright issues, enable snappy tagline

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
-set :application, "Brita"
-set :repository,  "git@github.com:kristjan/brita.git"
+set :application, "chlorine"
+set :repository,  "git@github.com:kristjan/#{application}.git"
 
 set :scm, :git
-set :deploy_to, "/var/www/brita"
+set :deploy_to, "/var/www/#{application}"
 set :use_sudo, false
 
-role :web, "brita.kripet.us"                   # Your HTTP server, Apache/etc
-role :app, "brita.kripet.us"                   # This may be the same as your `Web` server
-role :db,  "brita.kripet.us", :primary => true # This is where Rails migrations will run
-role :db,  "brita.kripet.us"
+role :web, "#{application}.kripet.us"                   # Your HTTP server, Apache/etc
+role :app, "#{application}.kripet.us"                   # This may be the same as your `Web` server
+role :db,  "#{application}.kripet.us", :primary => true # This is where Rails migrations will run
+role :db,  "#{application}.kripet.us"
 
 default_environment["PATH"] = "/var/lib/gems/1.8/bin:/usr/local/bin:/usr/bin:/bin"
 
