72a3f6f0d43fbe0cb62d0ef9bc92317174d729fa
Kristján <kristjan@gmail.com>
2010-04-03 21:09:08 -0700

    Let us develop without talking to Facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ private
   end
 
   def current_user
+    return Employee.first if ENV['IGNORE_CONNECT'] == 'true'
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.employee
   end
