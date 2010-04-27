38174ed40b940f4367630a71a8ee7c233434c4f1
Kristján <kristjan@gmail.com>
2010-04-27 04:30:11 -0700

    Set up a Mailer, no content yet

---
+++ app/models/megaphone.rb
@@ -0,0 +1,31 @@
+class Megaphone < ActionMailer::Base
+
+
+  def new_recruit(sent_at = Time.now)
+    subject    'Megaphone#new_recruit'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+
+    body       :greeting => 'Hi,'
+  end
+
+  def daily_report(sent_at = Time.now)
+    subject    'Megaphone#daily_report'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+
+    body       :greeting => 'Hi,'
+  end
+
+  def test(sent_at = Time.now)
+    subject    'Megaphone#test'
+    recipients 'kristjan@gmail.com'
+    from       'Chlorine'
+    sent_on    sent_at
+
+    body       :greeting => 'Hi,'
+  end
+
+end
